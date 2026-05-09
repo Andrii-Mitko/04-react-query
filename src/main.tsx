4bf59e1b5fce2985674db69302dadf8902125b25
Andrii-Mitko <andreymit123@gmail.com>
2026-05-09 15:18:26 +0300

    npm

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,16 @@
-import { createRoot } from "react-dom/client";
+
 import App from "./components/App/App";
 import "modern-normalize";
 import "./index.css";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-createRoot(document.getElementById("root") as HTMLDivElement).render(<App />);
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+);
