4ba80a584706c1094c2f8ebc180035e33380cd9e
Andrii-Mitko <andreymit123@gmail.com>
2026-05-09 20:00:17 +0300

    replace custom pagination with react-paginate

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -22,6 +22,3 @@ export default function Pagination({ totalPages, page, setPage }: Props) {
     />
   );
 }
-
-// generate conventional commit message with scope and description based on my changes
-// commit message: refactor: replace custom pagination with react-paginate
